a7428704217fd366b2c5a0ec7b72066b272b6958
rczerwiec <czerwiecr1999@gmail.com>
2024-06-09 14:04:39 +0200

    first commit

---
+++ src/main/java/pl/stylowamc/smcore/SMCore.java
@@ -0,0 +1,17 @@
+package pl.stylowamc.smcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
