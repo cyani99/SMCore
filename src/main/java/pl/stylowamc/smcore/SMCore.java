d4a3dfb87cc615920fb6f6b7382b4210ea5efda8
rczerwiec <czerwiecr1999@gmail.com>
2024-06-10 19:46:27 +0200

    CHANGE: opening inventory added - smith panel

--- src/main/java/pl/stylowamc/smcore/SMCore.java
+++ src/main/java/pl/stylowamc/smcore/SMCore.java
@@ -4,6 +4,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import pl.stylowamc.smcore.gameplay.tools.ToolsCraftingBlock;
 import pl.stylowamc.smcore.gameplay.tools.commands.GetTools;
+import pl.stylowamc.smcore.gameplay.tools.commands.SmithItem;
+import pl.stylowamc.smcore.gameplay.tools.gui.SmithGUI;
+
+//https://www.youtube.com/watch?v=D67hjtrxI4E&t=135s to check
 
 public final class SMCore extends JavaPlugin {
     private static SMCore plugin;
@@ -14,10 +18,12 @@ public final class SMCore extends JavaPlugin {
 
         //EVENTS
         Bukkit.getPluginManager().registerEvents(new ToolsCraftingBlock(), this);
+        Bukkit.getPluginManager().registerEvents(new SmithGUI(), this);
 
 
         //COMMANDS
         this.getCommand("gettools").setExecutor(new GetTools());
+        this.getCommand("ulepsz").setExecutor(new SmithItem());
     }
 
     @Override
--- src/main/java/pl/stylowamc/smcore/gameplay/tools/commands/SmithItem.java
+++ src/main/java/pl/stylowamc/smcore/gameplay/tools/commands/SmithItem.java
@@ -1,2 +1,29 @@
-package pl.stylowamc.smcore.gameplay.tools.commands;public class SmithItem {
+package pl.stylowamc.smcore.gameplay.tools.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import pl.stylowamc.smcore.gameplay.tools.gui.SmithGUI;
+import pl.stylowamc.smcore.gameplay.tools.utils.WoodenPickaxe;
+
+public class SmithItem implements CommandExecutor {
+
+    // This method is called, when somebody uses our command
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            SmithGUI gui = new SmithGUI();
+            if (p.hasPermission("smcore.admin.smithitem")) {
+                if (args.length == 0) {
+                    gui.openInventory(p);
+                } else{
+                    p.sendMessage("Błąd");
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
 }
